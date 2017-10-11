086e2b2dadf45c3f6db2193b856b1610026077de
Madeline Stevens <stevensm1987@gmail.com>
2017-10-11 15:37:35 -0700

    reinstalled node modules

--- lab-maddy/sluggram-backend/src/lib/util.js
+++ lab-maddy/sluggram-backend/src/lib/util.js
@@ -53,20 +53,20 @@ export const s3UploadMulterFileAndClean = (data) => {
   // allways remove file and either pass on failure or success
   .catch(err => fs.remove(data.path).then(() => {throw err}))
   .then(s3Data => fs.remove(data.path).then(() => s3Data))
-}
+};
 
 export const s3DeletePhotoFromURL = (url) => {
-  let [Key] = url.split('/').slice(-1)
-  return s3.deleteObject({ Key, Bucket: process.env.AWS_BUCKET}).promise()
-}
+  let [Key] = url.split('/').slice(-1);
+  return s3.deleteObject({ Key, Bucket: process.env.AWS_BUCKET}).promise();
+};
 
 export const pagerCreate = (model, populate='') => (req, query={}) => {
-  let offset = (Number(req.query.page) - 1) || 0
-  let itemLimit = 100
-  let route = `${process.env.API_URL}/${model.modelName}s?page=`
+  let offset = (Number(req.query.page) - 1) || 0;
+  let itemLimit = 100;
+  let route = `${process.env.API_URL}/${model.modelName}s?page=`;
   return model.count()
   .then(count => {
-    let remaining = count - offset * itemLimit
+    let remaining = count - offset * itemLimit;
     return model.find(query)
     .populate(populate)
     .skip(offset > 0 ? offset * itemLimit : 0)
@@ -77,6 +77,6 @@ export const pagerCreate = (model, populate='') => (req, query={}) => {
       last: `${route}${Math.floor((count - 1) / itemLimit) + 1}`,
       prev: offset > 0 && remaining > 0  ? `${route}${offset}` : null,
       next: offset > -1 && remaining > itemLimit ? `${route}${offset + 2}` : null,
-    }))
-  })
-}
+    }));
+  });
+};
